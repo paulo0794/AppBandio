bc6cf07857dbdb12df8bcbacbd31b6352ef924d1
Paulo Henrique <paulo.oliveira438@etec.sp.gov.br>
2019-03-27 22:09:21 -0300

    Iniciei a criação do App Bandio

---
+++ app/src/main/java/br/com/etecia/appbandio/SingUpActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appbandio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sing_up_layout);
+    }
+}
